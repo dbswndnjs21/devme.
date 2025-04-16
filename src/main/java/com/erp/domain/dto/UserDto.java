dbc44e26e15f8efbc38fc12b13db328a8cd33e9b
Juwon <dbswndnjs21@naver.com>
2025-04-16 18:43:57 +0900

    [feat] 회원가입 기능

---
+++ src/main/java/com/erp/domain/dto/UserDto.java
@@ -0,0 +1,31 @@
+package com.erp.domain.dto;
+
+import com.erp.domain.entity.User;
+import com.erp.domain.enums.Role;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private String username;
+    private String password;
+    private String phone;
+    private Role role;
+    private String department;
+    private String position;
+
+    public User toEntity(String encodedPassword) {
+        return User.builder()
+                .username(username)
+                .password(encodedPassword) // 암호화된 비밀번호를 넣어줘야 함
+                .phone(phone)
+                .role(role)
+                .department(department)
+                .position(position)
+                .build();
+    }
+}
